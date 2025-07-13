e53f49092e5f3bc2f8732c46efeca88406ad387d
haradama <harada@sfc.wide.ad.jp>
2025-07-13 10:29:10 -0500

    Make run_demo.sh executable

--- examples/make_demo_biom.py
+++ examples/make_demo_biom.py
@@ -7,13 +7,13 @@ import numpy as np
 import biom
 from biom.util import biom_open   # thin h5py wrapper
 
-samples  = ["S1", "S2", "N1", "N2"]
+samples = ["S1", "S2", "N1", "N2"]
 features = ["ASV1", "ASV2", "ASV3"]
 
 data = np.array([
     [10,  0,  5,  2],
-    [ 0,  8,  1,  0],
-    [ 3,  3,  0,  7],
+    [0,  8,  1,  0],
+    [3,  3,  0,  7],
 ])
 
 table = biom.Table(
