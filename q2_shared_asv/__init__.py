e0bc8b15fe79c285b99c0e9911c8c24522a91cb0
haradama <harada@sfc.wide.ad.jp>
2025-07-13 00:40:57 -0500

    fix typo

--- q2_shared_asv/__init__.py
+++ q2_shared_asv/__init__.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2023, Biota Inc. All rights reserved.
+#
+# Distributed under the terms of the BSD 3-Clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import importlib
 
 __version__ = '0.2.0'
